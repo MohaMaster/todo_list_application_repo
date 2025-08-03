19597fe07ac24ea378cba6564bca6e25aec6a159
Mohamad Falyoun <mohamadfaekfalyoun@gmail.com>
2025-08-03 21:36:59 +0200

    fix error based on name

--- todo_list_main/urls.py
+++ todo_list_main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for todo_list_moha project.
+URL configuration for todo_list_main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- todo_list_main/wsgi.py
+++ todo_list_main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for todo_list_moha project.
+WSGI config for todo_list_main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_list_moha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_list_main.settings')
 
 application = get_wsgi_application()
